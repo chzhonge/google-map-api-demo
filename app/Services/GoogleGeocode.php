4a560e865117846b7c63e189288b95c3778b9c0d
chzhonge <chzhonge@gmail.com>
2020-11-09 15:16:50 +0800

    feat(service): add related to authorization method

--- app/Services/GoogleGeocode.php
+++ app/Services/GoogleGeocode.php
@@ -4,5 +4,25 @@ namespace App\Services;
 
 class GoogleGeocode
 {
+    private $api_key = null;
 
+    public function setAuthorizationKey($api_key)
+    {
+        if (empty($api_key)) {
+            throw new \InvalidArgumentException('please set the api key');
+        }
+
+        $this->api_key = $api_key;
+
+        return $this;
+    }
+
+    private function getAuthorizationKey()
+    {
+        if (is_null($this->api_key)) {
+            $this->api_key = env('GOOGLE_MAP_API_KEY', '');
+        }
+
+        return $this->api_key;
+    }
 }
